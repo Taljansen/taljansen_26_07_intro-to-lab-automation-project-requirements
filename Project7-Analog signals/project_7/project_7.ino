b3936db88a1ac76285c9113369ee7e09a5ae231a
Taljansen <hula.tallulah@gmail.com>
2026-07-28 13:35:07 +0300

    p7

--- Project7-Analog signals/project_7/project_7.ino
+++ Project7-Analog signals/project_7/project_7.ino
@@ -1,6 +1,8 @@
 
+#include <MD_PWM.h>
 
-
+// int ledPin = 4; 
+MD_PWM pwmLed(4);
 int rotaryPin = A0; // Input, read the knob
 int rotaryValue;
 
@@ -9,14 +11,17 @@ int rotaryValue;
 // library needs a number from 0 - 255 for the duty cycle. 0 means off, 255 means fully on, 128 means half.
 
 // knob gives: 0 to 1023
-// library 0 to 255
+// library 0 to 255 (max duty cycle)
+// 1023/4  
 
 void setup() {
   Serial.begin(9600);
+  pwmLed.begin(100);
 }
 
 void loop() {
   rotaryValue = analogRead(rotaryPin); //analogRead(A0) gives 0 - 1023 (voltage), 2.5 V - 512; 5 V - 1023
   Serial.println(rotaryValue);
   delay(200);
+  pwmLed.write(rotaryValue/4);
 }
